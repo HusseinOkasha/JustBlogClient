68554e1022b2cb36fe6cc7c93b21ee09854abadf
HusseinOkasha <husseinokasha13@gmail.com>
2022-03-20 13:44:59 +0200

    Deleting un needed comments

--- src/index.js
+++ src/index.js
@@ -3,19 +3,7 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-/*
-import { Provider } from "react-redux";
-import { createStore, compose, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./store/reducers/index";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
-*/
 ReactDOM.render(
   //<Provider store={store}>
   <React.StrictMode>
