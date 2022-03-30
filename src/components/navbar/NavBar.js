d67473c6c43f5f4e2064c06df18a4803222d7f9a
HusseinOkasha <husseinokasha13@gmail.com>
2022-03-30 17:21:10 +0200

    Changing navbar layout

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -6,15 +6,18 @@ function NavBar(props) {
   return (
     <Navbar bg="dark" variant="dark" sticky="top">
       <Container>
-        <Navbar.Brand href="#">JustBlog</Navbar.Brand>
         <Nav className="me-auto">
+          <Navbar.Brand href="#">JustBlog</Navbar.Brand>
           <Link className="nav-link" to="/">
             Home
           </Link>
           <Link className="nav-link" to="/new-post">
             New Post
           </Link>
-          <Link className="nav-link" to="/signup">
+        </Nav>
+
+        <Nav className="ms-auto">
+          <Link className="nav-link  " to="/signup">
             Signup
           </Link>
           <Link className="nav-link" to="/login">
